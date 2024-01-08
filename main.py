70451cd5c9a2b7bf354657e2b76c7e7f3611c07f
Dan <a@a.com>
2024-01-08 20:36:17 +0200

    task6

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect,url_for
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return "Hello world"
+
+calendar_entries = {
+    1: {'image': 'img1.jpg', 'text': 'Text pentru ziua 1'},
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html', calendar_entries=calendar_entries)
+
+@app.route('/edit/<int:day>', methods=['GET', 'POST'])
+def edit(day):
+    entry = calendar_entries.get(day)
+    if request.method == 'POST':
+        if 'image' in request.files:
+            image_file = request.files['image']
+            image_file.save(f"static/{image_file.filename}")
+            entry['image'] = f"static/{image_file.filename}"
+
+        entry['text'] = request.form['text']
+        return redirect(url_for('index'))
+    return render_template('edit.html', day=day, entry=entry)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
