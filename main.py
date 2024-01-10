4fd49c218eb60fae74c0d41da4de326da696d253
student <raul@telacad.ro>
2024-01-10 19:53:25 +0200

    Adaugare template homepage

--- main.py
+++ main.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 
 app = Flask(__name__)
 
-
+## TODO Actually pull entries out of the db
 calendar_entries = {
-    1: {'image': 'img1.jpg', 'text': 'Text pentru ziua 1'},
+    1: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 1'},
+    2: {'id':2, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 2'},
+    3: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 3'},
+    4: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 4'},
+    5: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 5'},
+    6: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 6'},
+    7: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 7'},
+    8: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 8'},
+    9: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 9'},
+    10: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 10'},
+    11: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 11'},
+    12:{'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 12'},
+    13: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 13'},
+    14: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 14'},
+    15: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 15'},
+    16: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 16'},
+    17: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 17'},
+    18: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 18'},
+    19: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 19'},
+    20: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 20'},
+    21: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 21'},
+    22: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 22'},
+    23: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 23'},
+    24: {'id':1, 'imagine': 'img1.jpg', 'mesaj': 'Text pentru ziua 24'},
 }
+
 
 @app.route("/css/<path:file>")
 def return_css(file):
@@ -13,7 +37,8 @@ def return_css(file):
 
 @app.route('/')
 def index():
-    return render_template('index.html', calendar_entries=calendar_entries)
+    list = calendar_entries.values()
+    return render_template('index.html', calendar_entries=list)
 
 @app.route('/edit/<int:day>', methods=['GET', 'POST'])
 def edit(day):
