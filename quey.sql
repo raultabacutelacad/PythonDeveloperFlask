ceeb5c12e5fbf8ecaa0c2ffe8348098c68474813
bde <bucaciuc.Daniela@gmail.com>
2024-01-10 18:28:50 +0100

    implemnatare task 3 creat tabel

---
+++ quey.sql
@@ -0,0 +1,9 @@
+create database `advent`;
+use `advent`;
+CREATE TABLE Ziua (
+    id INT PRIMARY KEY,
+    numar_zi INT,
+    imagine VARCHAR(255),
+    mesaj VARCHAR(255),
+    deschis BOOLEAN
+);
